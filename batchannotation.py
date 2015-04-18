3be46aa5ff168f6b3944694ffc6b6b310ba1e90f
Junyi Chai <junyic@princeton.edu>
2015-04-18 10:07:07 -0400

    Scripts to manually prepare training environment for opencv_traincascadeUse opencv_createsamples to generate more training cases than given

---
+++ batchannotation.py
@@ -0,0 +1,34 @@
+
+import subprocess
+import os
+
+fgdir = './fg/'
+bginfo = './bg/bg.txt'
+
+fg_files = os.listdir(fgdir)
+
+for i, i_file in enumerate(fg_files):
+    """
+    subprocess.call(["opencv_createsamples", "-img " + fgdir + i_file,
+                     "-bg " + bginfo, 
+                     "-info annotation%d.lst" %i,
+                     "-pngoutput",
+                     "-maxxangle 0",
+                     "-maxyangle 0",
+                     "-maxzangle 1"])
+    """
+    os.system("opencv_createsamples " + "-img " + fgdir + i_file + " "
+              + "-bg " + bginfo + " "
+              + "-info annotation%d.lst" %i + " "
+              + "-pngoutput "
+              + "-maxxangle 0 -maxyangle 0 -maxzangle 1 -h 20 -w 20")
+    os.system("mv *jpg ./tmp")
+    current_files = os.listdir("./tmp")
+    for which_current_file in current_files:
+        os.system("mv ./tmp/%s ./tmp/file%d_%s" %(which_current_file,
+                                                  i,
+                                                  which_current_file))
+    add_prefix_cmd = """awk '{ printf "file%d_"; print }' %s >>%s""" %(i, "annotation%d.lst" %i,    
+                                                                      "total.lst")
+    os.system(add_prefix_cmd)
+    os.system("mv ./tmp/*jpg ./processed")
