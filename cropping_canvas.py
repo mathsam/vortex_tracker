f4611121aa23cabd95128104ff5e480c84337a05
Xiaogang HE <hexg@princeton.edu>
2015-04-19 18:00:38 -0400

    Add function to draw rectange and move the rectange if the cursor is inside the box

--- cropping_canvas.py
+++ cropping_canvas.py
@@ -13,8 +13,6 @@ zone_codes["upper_left"] = 2
 zone_codes["upper_right"] = 3
 zone_codes["lower_left"] = 4
 zone_codes["lower_right"] = 5
-
-motion_codes = {}
     
 class CroppingCanvas(tk.Tk):
     def __init__(self, parent=None, width=500, height=500):
@@ -30,7 +28,7 @@ class CroppingCanvas(tk.Tk):
 
         self.crop_box_start_x = None #upper left corner x
         self.crop_box_start_y = None #upper left corner y
-        self.crop_box_end_x = None #bottom right corner x
+	self.crop_box_end_x = None #bottom right corner x
         self.crop_box_end_y = None #bottom right corner y
 
 	self._draw_image()
@@ -42,24 +40,46 @@ class CroppingCanvas(tk.Tk):
 
     def button_primary(self, event):
         positions = (event.x, event.y)
-        if self._get_zone_code(positions) == zone_codes['outside']:
-            self.canvas.delete(self.crop_box_obj)
-            self.crop_box_start = positions
-            self.motion_primary_zonecode = zone_codes['outside']
-            return
-        if self._get_zone_code(positions) == zone_codes['inside']:
-            self.motion_primary_zonecode = zone_codes['inside']
-            return
-        if self._get_zone_code(positions) == zone_codes['upper_left']:
+        
+        # create rectangle if not yet exist
+        if self.crop_box_obj is None:
+            self.crop_box_start_x = event.x
+	    self.crop_box_start_y = event.y
+            self.crop_box_obj = self.canvas.create_rectangle(self.x, self.y, 1, 1, fill="black")
+	    self.motion_primary_zonecode = zone_codes['outside']
+	    self.event_positions = positions
             return 
+
+        #self.motion_primary_zonecode = self._get_zone_code(positions)
+        self.motion_primary_zonecode = 0
+	self.event_positions = positions
+        return
         
     def motion_primary(self, event):
-        self.crop_box_start_x = event.x
-        self.crop_box_start_y = event.y
+        if self.motion_primary_zonecode == zone_codes['outside']:
+            curX, curY = (event.x, event.y)
+	    self.crop_box_start_x = self.event_positions[0]
+	    self.crop_box_start_y = self.event_positions[1]
+	    
+            # expand rectangle as you drag the mouse
+            self.canvas.coords(self.crop_box_obj, self.crop_box_start_x, self.crop_box_start_y, curX, curY)
+	    self.crop_box_end_x = curX
+	    self.crop_box_end_y = curY
+	    return
 
-        # create rectangle if not yet exist
-        #if not self.rect:
-        self.crop_box_obj = self.canvas.create_rectangle(self.x, self.y, 1, 1, fill="black")
+        if self.motion_primary_zonecode == zone_codes['inside']:
+	    dX = event.x - self.event_positions[0]
+	    dY = event.y - self.event_positions[1]
+	    self.canvas.coords(self.crop_box_obj, self.crop_box_start_x + dX, self.crop_box_start_y + dY,
+			                          self.crop_box_end_x + dX, self.crop_box_end_y + dY)
+	    '''self.crop_box_start_x += dX
+	    self.crop_box_start_y += dY
+	    self.crop_box_end_x += dX
+	    self.crop_box_end_y += dY'''
+	    return
+
+        if self.motion_primary_zonecode == zone_codes['upper_left']:
+            return
 
     def motion_over(self, event):
         if event.x < 50 or event.x > 150 or event.y < 20 or event.y > 80:
@@ -82,7 +102,25 @@ class CroppingCanvas(tk.Tk):
             lower_left:
             lower_right:
         """
-        return
+
+        ux = positions[0]
+        uy = positions[1]
+
+        if uy > border_top-10 and uy < border_top+10:
+            if ux > border_left-10 and ux < border_left+10: return 1
+            if ux > border_left+10 and ux < border_right-10: return 2
+            if ux > border_right-10 and ux < border_right+10: return 3
+            if ux > border_right+10 and ux < border_right+30: return 10
+
+        if uy > border_top+10 and uy < border_bottom-10:
+            if ux > border_left-10 and ux < border_left+10: return 4
+            if ux > border_left+10 and ux < border_right-10: return 5
+            if ux > border_right-10 and ux < border_right+10: return 6
+
+        if uy > border_bottom-10 and uy < border_bottom+10:
+           if ux > border_left-10 and ux < border_left+10: return 7
+           if ux > border_left+10 and ux < border_right-10: return 8
+           if ux > border_right-10 and ux < border_right+10: return 9
        
     def on_button_release(self, event):
         pass
