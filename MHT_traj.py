8453f00ef800c06d39cf88ff52fdb45a00a23144
Junyi Chai <junyic@princeton.edu>
2015-05-07 16:17:11 -0400

    update starting time

--- MHT_traj.py
+++ MHT_traj.py
@@ -1,3 +1,16 @@
+import pandas as pd
+
+traj_pd = pd.read_csv('traj.csv', sep=' ', header=None)
+traj = {}
+
+for i in range(0, len(traj_pd)):
+    id = int(traj_pd.iloc[i, 1])
+    num_frame = int(traj_pd.iloc[i, 0])
+    if traj.has_key(id):
+        traj[id].append([num_frame, traj_pd.iloc[i, 2], traj_pd.iloc[i, 3]])
+    else:
+        traj[id] = [[num_frame, traj_pd.iloc[i, 2], traj_pd.iloc[i, 3]]]
+
 colors_np = np.random.randint(0,255,(len(traj.keys()),3))
 colors = {}
 for i, id in enumerate(traj.keys()):
@@ -7,29 +20,39 @@ for i, id in enumerate(traj.keys()):
 import cv2
 import os
 
-fourcc = cv2.VideoWriter_fourcc('P','I','M','1')
-video = cv2.VideoWriter('./HAR_vortex_track.avi', fourcc, 20, (800, 600))
+f = scipy.io.netcdf_file('/home/junyic/Work/Courses/4th_year/DataSci/final/PV_anomaly1.nc', mode='r', mmap=True)
+PV = f.variables['PV_anomaly']
 
-for indx, i in enumerate(range(501, 1001)):
-    img = cv2.imread(os.getenv('HOME') + '/Dropbox/COS424_FinalProject/movie1/%d.png' %i)
+ZOOM_FACTOR = 3
+image_width = PV.shape[3]*ZOOM_FACTOR
+image_height = PV.shape[2]*ZOOM_FACTOR
+
+
+fourcc = cv2.VideoWriter_fourcc('P','I','M','1')
+video = cv2.VideoWriter('./HAR_vortex_traj.avi', fourcc, 20, (image_width, image_height))
+
+
+for indx, i in enumerate(range(400, 2000)):
     print i
-    #gray = img
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    PV_slice = PV[i,0,...]
+    PV_hires = scipy.ndimage.interpolation.zoom(PV_slice, 3, mode='wrap')
+    
+    mapper = cm.ScalarMappable(cmap=cm.Paired)
+    img = mapper.to_rgba(PV_hires, alpha=None, bytes=True)
 
     for id in traj:
         for positions in traj[id]:
             if positions[0] > indx:
                 continue
+            if positions[0] < indx - 100:
+                continue
             if len(traj[id])< 20:
                 continue
             x = int(positions[1])
             y = int(positions[2])
-            cv2.circle(img, (x,y), 5, colors[id].tolist(), -1)
+            cv2.circle(img, (x,y), 4, colors[id].tolist(), -1)
 
-    #cv2.imshow('vortex', img)
-    #plt.imshow(img, cmap='gray')
-    #plt.show()
-    video.write(img)
+    video.write(img[...,:3])
 
 
 video.release()
